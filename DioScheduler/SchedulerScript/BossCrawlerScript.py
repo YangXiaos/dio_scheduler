e9221676d6292ff64d361b8dda5789457446b16c
YangXiaos <178069857@qq.com>
2018-12-13 22:50:55 +0800

    boss 爬虫抓取脚本 fix catch 异常

--- DioScheduler/SchedulerScript/BossCrawlerScript.py
+++ DioScheduler/SchedulerScript/BossCrawlerScript.py
@@ -3,6 +3,12 @@
 # @File         : BossCrawlerScript.py
 # @Description  :
 import logging
+import traceback
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(levelname)s] : [%(asctime)s] : %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='/home/mryang/Project/dio_scheduler/test.log')
 import time
 
 import schedule
@@ -12,10 +18,7 @@ from DioSpider.OldSpider.Boss.BossJobSpider import BossJobSpider
 from DioSpider.OldSpider.Boss.BossSearchSpider import BossSearchSpider
 
 
-logging.basicConfig(level=logging.INFO,
-                    format='[%(levelname)s] : [%(asctime)s] : %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    filename='/home/mryang/Project/dio_scheduler/test.log')
+
 
 # 入口url
 ENTER_URL = "https://www.zhipin.com/c101280100/?query=%E7%88%AC%E8%99%AB&period=3&ka=sel-scale-3"
@@ -38,20 +41,28 @@ def run():
     logging.info("搜索爬虫跑数 结束")
 
     for msg in msgs:
-        rst = list(BossJobSpider().crawl(msg.getEnterUrl(), {}))
-        logging.info("写入数据 {}条")
-        writer.run(job, rst)
-        logging.info("写入成功")
+        enterUrl = msg.getEnterUrl()
+        try:
+            logging.info("处理url {}".format(enterUrl))
+            rst = list(BossJobSpider().crawl(enterUrl, {}))
+            logging.info("写入数据 {}条".format(len(rst)))
+            writer.run(job, rst)
+            logging.info("写入成功")
+        except Exception as e:
+            logging.error("{} 跑数失败".format(enterUrl))
+            traceback.print_exc()
     logging.info("跑数结束")
 
 
 def main():
-    schedule.every(1).hours.do(run)
     logging.info("调度程序开始")
+    schedule.every(1).hours.do(run)
+    run()
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 
 if __name__ == '__main__':
+
     main()
